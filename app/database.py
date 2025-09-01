2328b6e7d52eb61a8a63b8f6cb9d66e56533a1a1
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-09-01 13:34:41 +0100

    Added postgre url database link in database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os 
 
-SQLALCHEMY_DATABASE_URL = "x"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/quillstack"
 
 engine =create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
